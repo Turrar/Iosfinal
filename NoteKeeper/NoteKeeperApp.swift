51612c673c123dedea78b29869051232cc458c2d
2 <RAR>
2025-05-31 15:21:53 +0500

    Initial Commit

---
+++ NoteKeeper/NoteKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteKeeperApp.swift
+//  NoteKeeper
+//
+//  Created by Macbook Air on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
